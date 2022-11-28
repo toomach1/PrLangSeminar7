a2b0c1aae158ea0e8f4e1fe1488b71b84e8c8ee9
Artem <temazasport@mail.ru>
2022-11-28 12:16:39 +0800

    task3_3

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,45 +1,51 @@
 ﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
 // m = 3, n = 4.
 
-int rows = SetNumber("m");
-
-int column = SetNumber("n");
-
-int[,] matrixMN = GetMatrixMN(rows,column);
-
-PrintMatrix(matrixMN);
-
-int SetNumber(string str) //метод "введите Х"
+internal class Program
 {
-
-    System.Console.WriteLine($"Введите {str}");
-    int num = int.Parse(Console.ReadLine());
-
-    return num;
-}
-
-int[,] GetMatrixMN(int rowsMetod, int columnMetod)
-{
-    int[,] matrixMetod = new int[rows,column];
-   
-    for (int i = 0; i < matrixMetod.GetLength(0); i++)
+    private static void Main(string[] args)
     {
-        for (int j = 0; j < matrixMetod.GetLength(1); j++)
-        {
-            matrixMetod[i,j] =i+j;
-        }
-    }
-    return matrixMetod;
-}
+        int rows = SetNumber("m");
 
-void PrintMatrix(int[,] matrixMetod)
-{
-    for (int i = 0; i < matrixMetod.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrixMetod.GetLength(1); j++)
+        int column = SetNumber("n");
+
+        int[,] matrixMN = GetMatrixMN(rows, column);
+
+        PrintMatrix(matrixMN);
+
+        int SetNumber(string str) //метод "введите Х"
         {
-            System.Console.Write($"{matrixMN[i,j]} ");
+
+            Console.WriteLine($"Введите {str}");
+            int num = int.Parse(Console.ReadLine());
+
+            return num;
         }
-        System.Console.WriteLine();
+
+        int[,] GetMatrixMN(int rowsMetod, int columnMetod)
+        {
+            int[,] matrixMetod = new int[rows, column];
+
+            for (int i = 0; i < matrixMetod.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixMetod.GetLength(1); j++)
+                {
+                    matrixMetod[i, j] = i + j;
+                }
+            }
+            return matrixMetod;
+        }
+
+        void PrintMatrix(int[,] matrixMetod)
+        {
+            for (int i = 0; i < matrixMetod.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixMetod.GetLength(1); j++)
+                {
+                    Console.Write($"{matrixMN[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задайте двумерный массив. Найдите элементы, у которых оба индекса четные, замените эти элементы на их квадраты
+
+int rows = SetNumber("m");
+
+int column = SetNumber("n");
+
+int[,] matrix1 = GetRandomMatrix(rows, column);
+
+PrintMatrix(matrix1);
+
+System.Console.WriteLine();
+
+int[,] NewMatrix = ReternNewMatrix(matrix1);
+
+PrintMatrix(NewMatrix);
+
+int SetNumber(string str) //метод "введите Х"
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    int num = int.Parse(Console.ReadLine());
+
+    return num;
+}
+
+int[,] GetRandomMatrix(int rowsMetod, int columnMetod, int maxValue = 10, int minValue = 0)
+{
+    int[,] matrixMetod = new int[rows, column];
+    var random = new Random();
+    for (int i = 0; i < matrixMetod.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixMetod.GetLength(1); j++)
+        {
+            matrixMetod[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return matrixMetod;
+}
+
+int[,] ReternNewMatrix(int[,] matrixMetodRet)
+{
+    int[,] matrixRet = new int[matrixMetodRet.GetLength(0), matrixMetodRet.GetLength(1)];
+    for (int i = 0; i < matrixMetodRet.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixMetodRet.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrixRet[i, j] = matrixMetodRet[i, j] * matrixMetodRet[i, j];
+            }
+            else
+            {
+                matrixRet[i, j] = matrixMetodRet[i, j];
+            }
+        }
+    }
+    return matrixRet;
+}
+
+void PrintMatrix(int[,] matrixMetodPrint)
+{
+    for (int i = 0; i < matrixMetodPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixMetodPrint.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrixMetodPrint[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
